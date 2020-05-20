f8f29540c629b8bf697b0e26be91515728e02c02
Zhounning <1767508581@qq.com>
2020-05-20 16:17:49 +0800

    图片访问测试

--- src/main/java/com/xiaoba/config/MyWebMvcConfigurer.java
+++ src/main/java/com/xiaoba/config/MyWebMvcConfigurer.java
@@ -1,13 +1,16 @@
 package com.xiaoba.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author zhouning
  */
-public class MyConfig implements WebMvcConfigurer {
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,5 +19,13 @@ public class MyConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //这个将应用到url中
+        registry.addResourceHandler("/images/**")
+                //这里填的是图片的绝对父路径
+                .addResourceLocations("file:/home/images");
     }
 }
