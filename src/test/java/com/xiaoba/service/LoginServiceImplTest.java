3290e14e76cc4b340a9ebd64e86103675eecbfca
WWX1999 <2381679817@qq.com>
2020-05-28 18:38:46 +0800

    --story=1002307 --user=王文旭 登录和注册测试,修复无法运行测试的bug https://www.tapd.cn/58518241/s/1004794

--- src/test/java/com/xiaoba/service/LoginServiceImplTest.java
+++ src/test/java/com/xiaoba/service/LoginServiceImplTest.java
@@ -1,15 +1,13 @@
 package com.xiaoba.service;
 
 import com.xiaoba.bean.SysUser;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.util.Map;
-
-import static org.testng.Assert.*;
 
 @SpringBootTest
 public class LoginServiceImplTest extends AbstractTestNGSpringContextTests {
@@ -17,7 +15,7 @@ public class LoginServiceImplTest extends AbstractTestNGSpringContextTests {
     @Autowired
     LoginService loginService;
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testErroPassword(){
         System.out.print("开始测试登录时密码错误的情况......");
         Map<String,Object> map=loginService.login("user02","000000");
@@ -26,7 +24,7 @@ public class LoginServiceImplTest extends AbstractTestNGSpringContextTests {
         //System.out.print("测试结束......");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testErroName(){
         System.out.print("开始测试登录时用户不存在的情况......");
         Map<String,Object> map=loginService.login("111111","000000");
@@ -34,7 +32,7 @@ public class LoginServiceImplTest extends AbstractTestNGSpringContextTests {
         Assert.assertEquals(map.get("token"),"-1");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void testLogin() {
         System.out.print("开始测试正常登录......");
         Map<String,Object> map=loginService.login("user02","12345678");
